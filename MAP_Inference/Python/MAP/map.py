f07bfdb9a5216a07fde9eb46dc8ab65831e5c140
Vict0r-David <victor.david973@gmail.com>
2022-08-19 17:53:32 +0200

    Mise en forme map.py

--- MAP_Inference/Python/MAP/map.py
+++ MAP_Inference/Python/MAP/map.py
@@ -9,7 +9,10 @@ Last update 19/08/2022
 import json
 import time
 
-# Load the data
+
+##############################################################################################################
+##############################################################################################################
+####################################### LOAD the data and RESULTS ############################################
 
 #with open('.\..\..\Data_Json\Dictionnary\dicoConfNodes.json', 'r') as f:
 #with open('.\..\..\Data_Json\Dictionnary\\1kDico.json', 'r') as f:
@@ -21,9 +24,11 @@ with open('.\..\..\Data_Json\Dictionnary\\50Dico.json', 'r') as f:   # 0.0 s vs 
 #with open('.\..\..\Data_Json\Dictionnary\\10Dico.json', 'r') as f:   # 0.0 s vs 0.0 s    || 0.86158 vs 1.317489
     dico = json.load(f)
 
-##############################################################################################################
-##############################################################################################################
 
+
+##############################################################################################################
+##############################################################################################################
+###################################### First Solution - Algorithme 1 #########################################
 
 # Algo naive : first solution
 # output = (sum, list_of_nodes, list_of_conflicts)
@@ -33,8 +38,7 @@ start = time.time()
 output = [0,[],[]]
 for elem in dico.items():
     (k,v) = elem
-    #print(f'k: {k}, output[1]: {output[1]}, output[2]: {output[2]}, v[1]: {v[1]}')
-    if (int(k) not in output[2]) and (not((set(output[1]) & set(v[1])))): #and (int(k) not in v[1])
+    if (int(k) not in output[2]) and (not((set(output[1]) & set(v[1])))): 
         output[0] += v[0]
         output[1].append(int(k))
         output[2] += v[1]
@@ -50,8 +54,11 @@ print(f'Temps d\'exécution : {elapsed:.5}s')
 
 print('\n\n')
 
+
+
 ##############################################################################################################
 ##############################################################################################################
+########################################  Calcule TOUT - Algorithme 2 ########################################
 
 # Algo force brut : test any solution 
 output = [0,[]]
@@ -74,9 +81,6 @@ def max_solution(dico):
 #sol = max_solution(dico)
 #print(test_solution(dico,sol))
 
-##############################################################################################################
-##############################################################################################################
-
 # from a solution give the list of all solutions minus one id_node
 def list_of_subsolutions(solution):
     l = []
@@ -96,133 +100,13 @@ def best_solution(dico,solution,current):
         return solution
     else:
         l = list_of_subsolutions(solution)
-        #print(f'subliste: {l}')
         for sol in l:
             s = best_solution(dico,sol,current)
-            #print(f'courrante: {current}')
-            #print(f'{s}\n')
             if s not in current and s != current:
                 current.append(s)
         return current
         
 #print(best_solution(dico,max_solution(dico),[]))
-
-##############################################################################################################
-##############################################################################################################
-##  OPTI A FAIRE ##
-
-#def testBuildList(k,liste):
-#	if liste == []:
-#		return True
-#	for l in liste:
-#		if k in l:
-
-def sum_weight(dico,solution):
-    sum = 0
-    for id in solution:
-        sum += dico[str(id)][0]
-    return sum
-
-def max_sum_list_int(dico,l_sol):
-    l_sum = []
-    for sol in l_sol:
-        l_sum.append(sum_weight(dico,sol[0]))
-    return (max(l_sum), l_sol[l_sum.index(max(l_sum))])
-
-
-
-def deletInclude(liste):
-	i = 0
-	while i < len(liste):
-		j = i + 1 
-		while j < len(liste):
-			if set(liste[i][0]) < set(liste[j][0]):
-				del liste[i]
-			if set(liste[j][0]) < set(liste[i][0]):
-				del liste[j]	
-			j += 1
-		i += 1
-	return liste
-
-#liste = [[id_nodes],[conflicts]]
-def compatible_merge(node,liste,dico):
-	new = list(dico[str(node)][1])
-	l_merge_comp = [[node],new]
-	compatible = True
-	for n in liste[0]:
-		if node in dico[str(n)][1] :
-			compatible = False
-		else:
-			new2 = list(dico[str(n)][1])
-			l_merge_comp[0].append(n)
-			l_merge_comp[1] += new2
-			l_merge_comp[1] = list(set(l_merge_comp[1]))
-	
-	return (l_merge_comp,compatible)
-		
-
-
-# Build the solutions
-def build_sol(dico):
-	liste_sol = []
-	i = 0 
-	
-	for k, v in dico.items():
-		if i == 0:
-			new = list(dico[k][1])
-			liste_sol.append([[int(k)],new])
-			i = 1
-
-		else:
-			for j in range(0,len(liste_sol)):
-				(l2,bool) = compatible_merge(int(k),liste_sol[j],dico)
-				if bool:
-					liste_sol[j][0].append(int(k))
-					liste_sol[j][1] += new
-					liste_sol[j][1] = list(set(liste_sol[j][1]))
-				else:
-					liste_sol += [[l2[0],l2[1]]]
-			liste_sol = deletInclude(liste_sol)
-		
-	return liste_sol
-
-	
-	#list(set(output[2]))
-
-#print(build_sol(dico))
-
-start = time.time()
-
-print(max_sum_list_int(dico,build_sol(dico)))
-
-end = time.time()
-elapsed = end - start
-
-print(f'Temps d\'exécution : {elapsed:.5}s')
-
-
-#s = ['1','2','3','4']
-#print(list_of_subsolutions(s))
-# => [['2', '3', '4'], ['1', '3', '4'], ['1', '2', '4'], ['1', '2', '3']]
-
-def opti_best_solution(dico,solution,current):
-    if test_solution(dico,solution):
-        return solution
-    else:
-        l = list_of_subsolutions(solution)
-        #print(f'subliste: {l}')
-        for sol in l:
-            s = best_solution(dico,sol,current)
-            #print(f'courrante: {current}')
-            #print(f'{s}\n')
-            if s not in current and s != current:
-                current.append(s)
-        return current
-        
-#print(best_solution(dico,max_solution(dico),[]))
-
-##############################################################################################################
-##############################################################################################################
 
 #Return True if L1 is strictly include in L2, otherwhise False
 def isInclude(L1, L2):
@@ -269,3 +153,88 @@ end = time.time()
 elapsed = end - start
 
 #print(f'Temps d\'exécution : {elapsed:.5}s')
+
+
+##############################################################################################################
+##############################################################################################################
+########################################  OPTIMISATION - Algorithme 3 ######################################## 
+
+def sum_weight(dico,solution):
+    sum = 0
+    for id in solution:
+        sum += dico[str(id)][0]
+    return sum
+
+def max_sum_list_int(dico,l_sol):
+    l_sum = []
+    for sol in l_sol:
+        l_sum.append(sum_weight(dico,sol[0]))
+    return (max(l_sum), l_sol[l_sum.index(max(l_sum))])
+
+
+
+def deletInclude(liste):
+	i = 0
+	while i < len(liste):
+		j = i + 1 
+		while j < len(liste):
+			if set(liste[i][0]) < set(liste[j][0]):
+				del liste[i]
+			if set(liste[j][0]) < set(liste[i][0]):
+				del liste[j]	
+			j += 1
+		i += 1
+	return liste
+
+#liste = [[id_nodes],[conflicts]]
+def compatible_merge(node,liste,dico):
+	new = list(dico[str(node)][1])
+	l_merge_comp = [[node],new]
+	compatible = True
+	for n in liste[0]:
+		if node in dico[str(n)][1] :
+			compatible = False
+		else:
+			new2 = list(dico[str(n)][1])
+			l_merge_comp[0].append(n)
+			l_merge_comp[1] += new2
+			l_merge_comp[1] = list(set(l_merge_comp[1]))
+	
+	return (l_merge_comp,compatible)
+		
+# Build the solutions
+def build_sol(dico):
+	liste_sol = []
+	i = 0 
+	
+	for k, v in dico.items():
+		if i == 0:
+			new = list(dico[k][1])
+			liste_sol.append([[int(k)],new])
+			i = 1
+		else:
+			for j in range(0,len(liste_sol)):
+				(l2,bool) = compatible_merge(int(k),liste_sol[j],dico)
+				if bool:
+					liste_sol[j][0].append(int(k))
+					liste_sol[j][1] += new
+					liste_sol[j][1] = list(set(liste_sol[j][1]))
+				else:
+					liste_sol += [[l2[0],l2[1]]]
+			liste_sol = deletInclude(liste_sol)
+		
+	return liste_sol
+
+#print(build_sol(dico))
+
+start = time.time()
+
+print(max_sum_list_int(dico,build_sol(dico)))
+
+end = time.time()
+elapsed = end - start
+
+print(f'Temps d\'exécution : {elapsed:.5}s')
+
+##############################################################################################################
+##############################################################################################################
