cc76c2850033e263ed4c2eedbdd7dd46f5267f56
Vict0r-David <victor.david973@gmail.com>
2022-08-20 20:37:44 +0200

    listOfDico

---
+++ MAP_Inference/Python/Data_Manipulation/dicoToNdico.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 20/08/2022
+Last update 20/08/2022
+
+@author: Victor
+"""
+
+import json
+import time
+
+
+##############################################################################################################
+##############################################################################################################
+############################################## LOAD the data #################################################
+
+with open('.\..\..\Data_Json\Dictionnary\dicoConfNodes.json', 'r') as f: 	
+#with open('.\..\..\Data_Json\Dictionnary\\60Dico.json', 'r') as f: 
+    dico = json.load(f)
+
+ListeOfDicos = []
+
+for k,v in dico.items():
+    buildNewDico = True
+    for dico in ListeOfDicos:
+        for key,value in dico.items():
+            if int(k) in value[1]:
+                dico[k] = v 
+                buildNewDico = False
+                break
+    if buildNewDico:
+        ListeOfDicos.append({k:v})         
+        
+    #break
+
+#for dico in ListeOfDicos:
+#    print(f'{dico}')
+
+##############################################################################################################
+##############################################################################################################
+
+# Creation of the json file of this dictionnary    
+fichier = open("listOfDico.json", "w")
+
+i = 1
+
+fichier.write("{\n")
+fichier.write("\t \"list\": [\n")
+for dico in ListeOfDicos:
+    fichier.write("\t\t")
+
+    fichier.write("{")
+    size = len(dico)
+    j = 1
+    for (k,v) in dico.items():
+        fichier.write("\"")
+        fichier.write(str(k))
+        fichier.write("\": [")
+        fichier.write(str(v[0]))
+        fichier.write(", ")
+        fichier.write(str(v[1]))
+        if j != size:    
+            fichier.write("],")
+        else:
+            fichier.write("]")
+        j += 1
+    fichier.write("}")
+
+
+    if i < len(ListeOfDicos):
+        fichier.write(", \n")
+        i += 1
+    else:
+        fichier.write("\n")
+fichier.write("\t ]\n")
+fichier.write("}")
+
+fichier.close()    
