39bf8f5cd143f1d8bce66588889852d7e8b8c651
chewbii <chewbii@gmail.com>
2022-08-26 00:22:47 +0200

    Graph Modeling module

---
+++ Neo4j/wikidata/graphModeling/src/Connection.java
@@ -0,0 +1,189 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Config;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
+
+public class Connection implements AutoCloseable {
+	private final Driver driver;
+	private BufferedWriter log;
+
+	private String outputFolder;
+
+	public Connection() throws Exception {
+		Config config = Config.builder().build();
+		driver = GraphDatabase.driver((String) GraphModeling.config.get("URI"),
+				AuthTokens.basic((String) GraphModeling.config.get("user"), (String) GraphModeling.config.get("mdp")),
+				config);
+	}
+
+	@Override
+	public void close() throws Exception {
+		driver.close();
+		log.flush();
+		log.close();
+	}
+
+	public void initLog(String file) throws Exception {
+		outputFolder = "output/" + file + "/";
+		File f = new File(outputFolder);
+		f.mkdir();
+		log = new BufferedWriter(new FileWriter(outputFolder + "time.csv"));
+		log.write("STEP;QUERY;TIME (ms)\n");
+	}
+
+	public void log(String step, String query, long time) throws IOException {
+		System.out.println(step + "\t" + query + "\t" + time + " ms");
+		log.write(step + ";" + query + ";" + time);
+		log.newLine();
+		if (query.compareTo("TOTAL") == 0)
+			log.newLine();
+	}
+
+	public void updateQueries(String step, List<Query> queries) {
+		try (Session session = driver.session()) {
+			// int nb =
+			session.writeTransaction(new TransactionWork<Integer>() {
+				@Override
+				public Integer execute(Transaction tx) {
+					long total = 0l;
+					Instant start, end;
+					Duration timeElapsed;
+					for (Query q : queries) {
+						start = Instant.now();
+						tx.run(q.query);
+						end = Instant.now();
+						timeElapsed = Duration.between(start, end);
+						total += timeElapsed.toMillis();
+						try {
+							log(step, q.instruction, timeElapsed.toMillis());
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+					try {
+						log(step, "TOTAL", total);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					return 1;
+				}
+			});
+		} catch (org.neo4j.driver.exceptions.ServiceUnavailableException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void readQueries2CSV(String step, List<Query> queries, String csvFile) {
+		try (Session session = driver.session()) {
+			session.readTransaction(new TransactionWork<Integer>() {
+				@Override
+				public Integer execute(Transaction tx) {
+					long total = 0l;
+					Instant start, end;
+					Duration timeElapsed;
+					BufferedWriter output = null;
+					try {
+						output = new BufferedWriter(new FileWriter(outputFolder + csvFile+".csv"));
+						for (Query q : queries) {
+							start = Instant.now();
+							Result result = tx.run(q.query);
+							end = Instant.now();
+							timeElapsed = Duration.between(start, end);
+							log(step, q.instruction, timeElapsed.toMillis());
+							output.newLine();
+							for(String s : result.keys()) {
+								output.write(";"+s);
+							}
+							output.newLine();
+							while (result.hasNext()) {
+								output.write(q.instruction);
+								Record r = result.next();
+								for(int i=0;i<r.size();i++) {
+									output.write(";"+r.get(i));
+								}
+								output.newLine();
+							}
+						}
+						if(csvFile != null) {
+							output.flush();
+							output.close();
+						}
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					return 1;
+				}
+			});
+		}
+	}
+
+	public void readQueries2JSON(String step, List<Query> queries) {
+		try (Session session = driver.session()) {
+			session.readTransaction(new TransactionWork<Integer>() {
+				@Override
+				public Integer execute(Transaction tx) {
+					long total = 0l;
+					Instant start, end;
+					Duration timeElapsed;
+					BufferedWriter output = null;
+					try {
+						for (Query q : queries) {
+							int i = 0;
+							start = Instant.now();
+							Result result = tx.run(q.query);
+							end = Instant.now();
+							timeElapsed = Duration.between(start, end);
+							log(step, q.instruction, timeElapsed.toMillis());
+							
+							output = new BufferedWriter(new FileWriter(outputFolder + q.instruction+".json"));
+							output.write("[");
+							if(result.hasNext()) {
+								JSONObject o = toJSON(result.next());
+								output.write(o.toString());
+								
+								while (result.hasNext()) {
+									o = toJSON(result.next());
+									output.write(",\n"+o.toJSONString());
+								}
+							}
+							output.write("]");
+							output.flush();
+							output.close();
+						}
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					return 1;
+				}
+			});
+		}
+	}
+
+	private JSONObject toJSON (Record r) {
+		JSONObject o = new JSONObject ();
+		for(String k : r.keys()) {
+			o.put(k, r.get(k));
+		}
+
+		return o;
+	}
+}
---
+++ Neo4j/wikidata/graphModeling/src/GraphModeling.java
@@ -0,0 +1,165 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class GraphModeling {
+	private Connection connection;
+	public static String inputFile = null;
+
+	private static JSONParser parser = null;
+	public static JSONObject config;
+	
+	public String [][] replacements = {
+	};
+
+	public GraphModeling() throws Exception {
+		init ();
+	}
+
+	public void init () throws Exception {
+		config = readJSONFile("config.json");
+		connection = new Connection ();
+		connection.initLog((String)config.get(inputFile));
+	}
+
+	public void close () throws Exception {
+		connection.close();
+	}
+
+	public void graphSettings() throws Exception {
+		graphSetting ("schema.cypher");
+		graphSetting ("delete.cypher");
+		graphSetting ("graphSetting.cypher");
+		graphSetting ("inference.cypher");
+		graphSetting ("constraints.cypher");
+	}
+	
+	private void graphSetting (String fileQueries) {
+		System.out.println("----------------");
+		List<Query> queries = readQueries ((String)config.get("queriesFolder") + fileQueries);
+		for(Query q : queries) {
+			if(q.query.contains("<<FILE>>")) {
+				q.query = q.query.replace("<<FILE>>",inputFile);
+			}
+			for(String [] replacement : replacements) {
+				if(q.instruction.compareTo(replacement[0]) == 0) {
+					q.query = q.query.replace("<<"+replacement[1]+">>",(String)config.get(replacement[2]));
+				}	
+			}
+		}
+		connection.updateQueries(fileQueries, queries);
+	}
+	
+	private void statsQueries (String fileQueries, String csvFile) {
+		System.out.println("----------------");
+		List<Query> queries = readQueries ((String)config.get("queriesFolder") + fileQueries);
+		for(Query q : queries) {
+			for(String [] replacement : replacements) {
+				if(q.instruction.compareTo(replacement[0]) == 0) {
+					q.query = q.query.replace("<<"+replacement[1]+">>",(String)config.get(replacement[2]));
+				}	
+			}
+		}
+		connection.readQueries2CSV(fileQueries, queries, csvFile);
+	}
+
+	private void resultQueries (String fileQueries) {
+		System.out.println("----------------");
+		List<Query> queries = readQueries ((String)config.get("queriesFolder") + fileQueries);
+		for(Query q : queries) {
+			for(String [] replacement : replacements) {
+				if(q.instruction.compareTo(replacement[0]) == 0) {
+					q.query = q.query.replace("<<"+replacement[1]+">>",(String)config.get(replacement[2]));
+				}	
+			}
+		}
+		connection.readQueries2JSON(fileQueries, queries);
+	}
+
+	private List<Query> readQueries (String queryFile){
+		List<Query> queries = new ArrayList<Query> ();
+
+		try {
+			BufferedReader br = new BufferedReader (new FileReader (queryFile));
+			String line;
+			String instruction = "";
+			String query = "";
+			while( (line = br.readLine()) != null) {
+				if(line.startsWith("//")) {
+					instruction = line.substring(2);
+				} else if (line.endsWith(";")) {
+					if(query.length() > 0)
+						query += "\n\t";
+					query += line;
+					queries.add(new Query (instruction, query));
+					query = "";
+				} else if (line.length() > 0){
+					if(query.length() > 0)
+						query += "\n\t";
+					query += line;
+				}
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return queries;
+	}
+
+
+	public JSONObject readJSONFile(String file) throws Exception {
+		if(parser != null)
+			parser.reset();
+		else
+			parser = new JSONParser();
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		Object obj = parser.parse(br);
+		return (JSONObject) obj;
+	}
+
+	public static void main (String [] args) {
+		try {
+			boolean set = true;
+			boolean noDelete = false;
+			if(args.length > 0) {
+				try {
+					for(String s : args) {
+						if(s.startsWith("--inputFile=")) {
+							inputFile = s.substring(12);
+						} else if(s.startsWith("--help")) {
+							set = false;
+						} else if(s.startsWith("--noDelete")) {
+							noDelete = true;
+						}
+					}
+				} catch (Exception e) {
+					set = false;
+				}
+			}
+			
+			
+			if(!set || inputFile == null){
+				System.out.println("--inputFile=XXX.csv  -> evidences that have to be imported. Files must be put in the \"import\" neo4j folder of your database. It removes the previous database and create a new one with the input data.\n"
+						+ "--noDelete  -> apply directly queries without delete existing nodes\n"
+						+ "Query files are in the folder \"cypherQueries\".");
+				System.exit(0);
+			}
+
+			GraphModeling gm = new GraphModeling ();
+			if(!noDelete)
+				gm.graphSettings();
+			gm.statsQueries ("stats.cypher", "stats");
+			gm.resultQueries ("conflicts.cypher");
+			gm.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
