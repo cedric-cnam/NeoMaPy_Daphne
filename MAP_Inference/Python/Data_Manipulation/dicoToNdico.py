ce384ca9e9afe05edea0413819c46558c2bce555
Vict0r-David <victor.david973@gmail.com>
2022-08-29 08:58:25 +0200

    update dicoToNdico

--- MAP_Inference/Python/Data_Manipulation/dicoToNdico.py
+++ MAP_Inference/Python/Data_Manipulation/dicoToNdico.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on 20/08/2022
-Last update 20/08/2022
+Last update 28/08/2022
 
 @author: Victor
 """
@@ -15,34 +15,135 @@ import time
 ############################################## LOAD the data #################################################
 
 #with open('.\..\..\Data_Json\Dictionnary\dicoConfNodes.json', 'r') as f: 	
-with open('.\..\..\Data_Json\Dictionnary\\1kDico.json', 'r') as f: 
-    dico = json.load(f)
+#with open('.\..\..\Data_Json\Dictionnary\\1kDico.json', 'r') as f: 
+#with open('.\..\..\Data_Json\Dictionnary\ClearDico\dico-2.5kConf.json', 'r') as f: 
+with open('.\dico-2.5kConf.json', 'r') as f:     
+    dic = json.load(f)
 
-ListeOfDicos = []
+def oneDico(key, dico, l_id, d_out):
+    for n in dico[str(key)][1]:
+        if n not in l_id:
+            l_id += [int(n)]
+            d_out[str(n)]= dico[str(n)]
+            l_id,d_out = oneDico(str(n), dico, l_id, d_out)
+    return l_id,d_out
 
+"""
+test_d = {
+    "1": [0.17849, [2,3]],
+    "2": [0.19241, [1,3]],
+    "3": [0.25549, [1,2,6]],
+    "4": [0.19522999, [5]],
+    "5": [0.19223, [4]],
+    "6": [0.14644, [3]]
+    }
+"""
+"""
+def increase(dico):
+    l_deso = list(dico.items())
+    l_croi = [l_deso[0]]
+    for i in range(1,len(l_deso)):
+        add = False
+        for j in range (0,len(l_croi)):
+            if len(l_deso[i][1][1]) <= len(l_croi[j][1][1]):
+                l_croi.insert(j,l_deso[i])
+                add = True
+                break
+        if add == False:
+            l_croi.insert(j+1,l_deso[i])
+    dico_output = {}
+    for elem in l_croi:
+        dico_output[elem[0]] = elem[1]
+    return dico_output
+"""
+
+def decrease(dico):
+    l_deso = list(dico.items())
+    l_croi = [l_deso[0]]
+    for i in range(1,len(l_deso)):
+        add = False
+        for j in range (0,len(l_croi)):
+            if len(l_deso[i][1][1]) >= len(l_croi[j][1][1]):
+                l_croi.insert(j,l_deso[i])
+                add = True
+                break
+        if add == False:
+            l_croi.insert(j+1,l_deso[i])
+    dico_output = {}
+    for elem in l_croi:
+        dico_output[elem[0]] = elem[1]
+    return dico_output
+
+#l_id,d_out = oneDico(1, test_d, [1], {"1":[0.17849, [2,3]]})
+#print(f'l_id = {l_id}\n')
+#print(f'd_out = {d_out}\n')
+
+def Ndico(dico):
+    l_id_total = set()
+    d_total = []    
+    for k in dico:
+        if int(k) not in l_id_total:
+            d_out = {}
+            d_out[str(k)] = dico[str(k)]
+            l_id, d_out = oneDico(k, dico, [int(k)], d_out)
+            d_total.append(decrease(d_out))
+            l_id_total.update(set(l_id))
+
+    return d_total
+
+#print(Ndico(test_d))
+
+
+
+"""
+def decrease(dico):
+    l_deso = list(dico.items())
+    l_croi = [l_deso[0]]
+    for i in range(1,len(l_deso)):
+        add = False
+        for j in range (0,len(l_croi)):
+            if len(l_deso[i][1][1]) >= len(l_croi[j][1][1]):
+                l_croi.insert(j,l_deso[i])
+                add = True
+                break
+        if add == False:
+            l_croi.insert(j+1,l_deso[i])
+    return l_croi
+"""
+
+
+
+"""
 for k,v in dico.items():
     buildNewDico = True
     for dico in ListeOfDicos:
         for key,value in dico.items():
             if int(k) in value[1] or (set(v[1]) & set(value[1])):
+            #set(v[1]).intersection(set(value[1])) != set():
                 dico[k] = v 
                 buildNewDico = False
                 break
+        if buildNewDico == False:
+            break
     if buildNewDico:
         ListeOfDicos.append({k:v})         
-        
-"""
+
+
 list_size = []
 for dico in ListeOfDicos:
     list_size.append(len(dico))
 print(f'list = {list_size}')
+
 """
 
 ##############################################################################################################
 ##############################################################################################################
 
+
 # Creation of the json file of this dictionnary    
-fichier = open("listOfDico1k.json", "w")
+fichier = open("listOfDico2.5kClear.json", "w")
+
+ListeOfDicos = Ndico(dic)
 
 i = 1
 
@@ -50,7 +151,7 @@ fichier.write("{\n")
 fichier.write("\t \"list\": [\n")
 for dico in ListeOfDicos:
     fichier.write("\t\t")
-
+    #print(dico)
     fichier.write("{")
     size = len(dico)
     j = 1
---
+++ MAP_Inference/Python/Data_Manipulation/divideDico.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 17/08/2022
+Last update 18/08/2022
+
+@author: Victor
+"""
+
+import time
+import json
+
+
+# Give your initial json file containing the conflicting nodes
+data = 'dico-2.5kinit.json'
+with open(data, 'r') as f:
+    dico = json.load(f)
+
+
+##############################################################################################################
+
+# Creation of the json file of this dictionnary    
+file1 = open("dico-2.5kNoConf.json", "w")
+file1.write("{\n")
+
+file2 = open("dico-2.5kConf.json", "w")
+file2.write("{\n")
+
+i = 0
+j = 0
+
+for (k,v) in dico.items():
+    if v[1] == []:
+        if i != 0:    
+            file1.write("],\n ")
+        file1.write("\t\"")
+        file1.write(str(k))
+        file1.write("\": [")
+        file1.write(str(dico[str(k)][0]))
+        file1.write(", ")
+        file1.write(str(v[1]))
+        i += 1
+    else:
+        if j != 0:    
+            file2.write("],\n ")
+        file2.write("\t\"")
+        file2.write(str(k))
+        file2.write("\": [")
+        file2.write(str(dico[str(k)][0]))
+        file2.write(", ")
+        file2.write(str(v[1]))
+        j += 1
+
+file1.write("]\n")
+file1.write("}")
+file1.close()
+
+file2.write("]\n")
+file2.write("}")
+file2.close()
