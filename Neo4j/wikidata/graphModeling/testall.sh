f96c677ce4fe9665672a289844453fc1d320efb4
chewbii <chewbii@gmail.com>
2022-09-09 00:32:02 +0200

    Update testall.sh

--- Neo4j/wikidata/graphModeling/testall.sh
+++ Neo4j/wikidata/graphModeling/testall.sh
@@ -1,5 +1,6 @@
-for f in $(ls ~/Library/Application\ Support/Neo4j\ Desktop/Application/relate-data/dbmss/dbms-3b0e1ab2-d23e-4f05-a18e-78c491411645/import);\
+for f in $(ls /var/lib/neo4j/import);\
 do \
   java -jar graphModeling.jar --inputFile=$f --inference=false; \
   java -jar graphModeling.jar --inputFile=$f --inference=true --noDelete; \
+  sleep 5; \
 done;
